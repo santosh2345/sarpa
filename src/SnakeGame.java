5a6b8662f61465fe0469337f56e243abe054d21c
santosh2345 <santoshgiri2345@gmail.com>
2024-12-20 22:43:14 +0545

    added key event

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class SnakeGame extends JPanel implements ActionListener{
+public class SnakeGame extends JPanel implements ActionListener, keyListener{
 
     private class Tile {
         int x;
@@ -31,6 +31,8 @@ public class SnakeGame extends JPanel implements ActionListener{
     //game logic
 
     Timer gameLoop;
+    int velocityX;
+    int velocityY;
 
     SnakeGame(int boardWidth, int boardHeight){
         this.boardWidth = boardWidth;
@@ -44,6 +46,9 @@ public class SnakeGame extends JPanel implements ActionListener{
         food = new Tile(20,20);
         random = new Random();
         placeFood();
+
+        velocityX = 0;
+        velocityY = 0;
 
         gameLoop = new Timer(100, this);
         gameLoop.start();
@@ -78,8 +83,50 @@ public class SnakeGame extends JPanel implements ActionListener{
         food.y = random.nextInt(boardHeight/tileSize);
     }
 
+    public void move(){
+        snakeHead.x += velocityX;
+        snakeHead.y += velocityY;
+
+        
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        move();
        repaint();
     }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_UP){
+            velocityX = 0;
+            velocityY = -1;
+        }
+
+        if(key == KeyEvent.VK_DOWN){
+            velocityX = 0;
+            velocityY = 1;
+        }
+
+        if(key == KeyEvent.VK_LEFT){
+            velocityX = -1;
+            velocityY = 0;
+        }
+
+        if(key == KeyEvent.VK_RIGHT){
+            velocityX = 1;
+            velocityY = 0;
+        }
+       
+    }
+
+
+    // we don't need to implement the other methods of the interface
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
