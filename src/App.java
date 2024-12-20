4bafb03d05dabd0131c95a2750a4efe37db1a35d
santosh2345 <santoshgiri2345@gmail.com>
2024-12-20 20:24:29 +0545

    initialize the project with jpanel and swing

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+
+        JFrame frame = new JFrame("Hareu");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
