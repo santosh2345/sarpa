5301c6a03fc323be8cb2d1368864604b27875ed1
santosh2345 <santoshgiri2345@gmail.com>
2024-12-20 20:39:28 +0545

    build snakegame class extending the jpanel

--- src/App.java
+++ src/App.java
@@ -14,5 +14,8 @@ public class App {
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+
     }
 }
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -11,6 +11,8 @@ public class SnakeGame extends JPanel{
     SnakeGame(int boardWidth, int boardHeight){
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.BLACK);
     }
     
 }
