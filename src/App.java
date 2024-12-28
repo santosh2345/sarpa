458f5e57aa99095a92ef64bed7d3560392376c87
santosh2345 <santoshgiri2345@gmail.com>
2024-12-28 18:17:06 +0545

    commit

--- src/App.java
+++ src/App.java
@@ -3,24 +3,5 @@ import javax.swing.*;
 
 
 public class App {
-    public static void main(String[] args) throws Exception {
-
-        int boardWidth = 600;
-        int boardHeight = boardWidth;
-       
-
-
-
-        JFrame frame = new JFrame("Hareu");
-        frame.setVisible(true);
-        frame.setSize(boardWidth, boardHeight);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
-        frame.add(snakeGame);
-        frame.pack();
-        snakeGame.requestFocus();
-    }
+   
 }
